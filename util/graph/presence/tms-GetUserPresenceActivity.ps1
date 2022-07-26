cbbbce5a14de2b5b9c60f729f72c4f7188eedd73
Mihai Filip (Lugera A-mihaifilip Makler Romania) <a-mihaifilip@microsoft.com>
2022-07-26 10:51:17 +0300

    add disclaimer

--- util/graph/presence/tms-GetUserPresenceActivity.ps1
+++ util/graph/presence/tms-GetUserPresenceActivity.ps1
@@ -1,3 +1,11 @@
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+# THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED “AS IS” WITHOUT
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR 
+# RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+
 # get a list of users that have Teams
 $users = get-csonlineuser | ? {$_.assignedplan.capability -contains 'Teams'} | select userprincipalname, identity
 $presences = @()
