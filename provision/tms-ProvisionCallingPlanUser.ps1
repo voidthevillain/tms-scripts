01e71285e5b0dd0748752581951ccce3096f2910
Mihai Filip (Lugera A-mihaifilip Makler Romania) <a-mihaifilip@microsoft.com>
2022-08-04 15:37:34 +0300

    Added disclaimer and usage

--- provision/tms-ProvisionCallingPlanUser.ps1
+++ provision/tms-ProvisionCallingPlanUser.ps1
@@ -1,4 +1,22 @@
 # ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+# THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED “AS IS” WITHOUT
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR 
+# RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+# AUTHOR: Mihai Filip
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+# DEPENDENCIES: Connect-MsolService, Connect-MicrosoftTeams
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+# USAGE: 
+# Connect-MsolService
+# Connect-MicrosoftTeams
+# cd PATH_TO_SCRIPT
+# .\tms-ProvisionCallingPlanUser.ps1 -UserPrincipalName user@tms-ninja.com -DisplayName userOne -FirstName user -LastName one -PhoneNumber +40744XXXXXX
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
+
+# ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
 # PARAMS
 # ...- --- .. -.. - .... . ...- .. .-.. .-.. .- .. -. 
 
